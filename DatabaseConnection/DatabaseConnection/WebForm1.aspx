2567debab81d394ad4be7e52d0ea4595c9ab21bd
Ali Ahmed <aliahmedaiub@gmail.com>
2015-10-11 00:28:40 -0700

    First Commit

---
+++ DatabaseConnection/DatabaseConnection/WebForm1.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="DatabaseConnection.WebForm1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:gridview ID="Gridview1" runat="server" CellPadding="4" ForeColor="#333333" 
+            GridLines="None">
+        <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+        <EditRowStyle BackColor="#999999" />
+        <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+        <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+        <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+        <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+        <SortedAscendingCellStyle BackColor="#E9E7E2" />
+        <SortedAscendingHeaderStyle BackColor="#506C8C" />
+        <SortedDescendingCellStyle BackColor="#FFFDF8" />
+        <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+        </asp:gridview>
+    <br />
+    Name:<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox><br />
+    Email:<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />
+    Salary:<asp:TextBox ID="TextBox3" runat="server"></asp:TextBox><br />
+    Dept:<asp:DropDownList ID="DropDownList1" runat="server">
+        </asp:DropDownList>
+        <br />
+    <asp:Button ID="Button1" runat="server" Text="Button" onclick="Button1_Click" /><br />
+       <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </div>
+    
+    </form>
+</body>
+</html>
+
---
+++ DatabaseConnection/DatabaseConnection/WebForm1.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace DatabaseConnection
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                load();
+                this.loaddept();
+            }
+        }
+        public void load()
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connStr);
+            string sql = "SELECT e.id,e.name,e.salary,d.name,d.location FROM Employees e,Departments d WHERE e.deptid=d.id";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            conn.Open();//must be open
+            SqlDataReader reader = cmd.ExecuteReader();
+            /*while (reader.Read())
+            {
+                Response.Write(reader["name"].ToString());
+            }*/
+            Gridview1.DataSource = reader;
+            Gridview1.DataBind();
+            conn.Close();
+        }
+        public void loaddept()
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connStr);
+            string sql = "SELECT * from Departments";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            conn.Open();//must be open
+            SqlDataReader reader = cmd.ExecuteReader();
+            DropDownList1.DataSource = reader;
+            DropDownList1.DataTextField = "name";
+            DropDownList1.DataValueField = "id";
+            DropDownList1.DataBind();
+            conn.Close();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text == "" || TextBox2.Text == "" || TextBox3.Text == "" )
+            {
+                Label1.Text = "Invalid";
+                return;
+            }
+            double salary;
+            try
+            {
+                salary = Convert.ToDouble(TextBox3.Text);
+            }
+            catch (Exception ex)
+            {
+                Label1.Text = "Invalid salary";
+            }
+
+            string connStr = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connStr);
+            string sql = "INSERT INTO Employees(name,email,salary,deptid) values('"+TextBox2.Text+"','"+TextBox1.Text+"',"+TextBox3.Text+","+DropDownList1.SelectedValue+")";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            Label1.Text = "Data Add successfully";
+            this.load();
+
+        }
+
+    }
+}
